ce527d6ff564b3e639fc12b1d0f55a800557efb3
greatgabbar <strivedi_be19@thapar.edu>
2022-03-08 19:25:25 +0530

    some changes

--- src/components/AsciiImage.tsx
+++ src/components/AsciiImage.tsx
@@ -1,34 +1,52 @@
 import p5 from "p5";
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import img from "../../test.jpg";
+import imgURL from "../../test.jpg";
 
 interface AsciiImageProps {}
 
 export const AsciiImage: React.FC<AsciiImageProps> = ({}) => {
   const ref = useRef<HTMLDivElement>(null);
   const [canvas, setCanvas] = useState(null);
+  const [img, setImage] = useState(null);
 
-  const sketch = useCallback((p) => {
-    let img;
-    p.prelaod = () => {
-      img = p.loadImage("../../test.jpg");
-    };
-    p.setup = () => {
-      p.image(img, 0, 0);
-      p.createCanvas(400, 400);
-    };
+  useEffect(() => {
+    console.log("img", img);
+  }, [img]);
 
-    p.draw = () => {
-      //   p.background(220);
-      //   p.ellipse(50, 50, 80, 80);
-    };
-  }, []);
+  const sketch = useCallback(
+    (p) => {
+      // p.prelaod = () => {
+      //   img = p.loadImage("../../test.jpg");
+      // };
+      p.setup = () => {
+        p.createCanvas(1000, 1000);
+        if (!img) {
+          p.loadImage(imgURL, (img) => {
+            setImage(img);
+            p.redraw();
+          });
+        }
+        if (img) {
+          p.image(img, 0, 0);
+          p.loadPixels();
+          console.log("pixels", p.pixels);
+        }
+      };
+
+      p.draw = () => {
+        p.background(220);
+        p.ellipse(50, 50, 80, 80);
+        console.log(img);
+      };
+    },
+    [img]
+  );
 
   useEffect(() => {
     if (!ref.current) return;
 
     const canvas = new p5(sketch, ref.current);
-  }, []);
+  }, [img]);
 
   return (
     <div>
